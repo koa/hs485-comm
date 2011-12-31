0c95a35231a33fe97c129c7b12b99e8f03ee4938
Andreas Koenig <andreas.koenig@berg-turbenthal.ch>
2011-12-31 09:10:02 +0100

    used queues for thread-synchronization

--- src/main/java/ch/eleveneye/hs485/protocol/HS485Impl.java
+++ src/main/java/ch/eleveneye/hs485/protocol/HS485Impl.java
@@ -7,11 +7,17 @@ import gnu.io.UnsupportedCommOperationException;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,7 +31,7 @@ import ch.eleveneye.hs485.event.EventHandler;
 import ch.eleveneye.hs485.protocol.handler.RawDataHandler;
 
 public class HS485Impl implements HS485 {
-	class AckRunnable implements Runnable {
+	private class AckRunnable implements Runnable {
 		IMessage	origMessage;
 
 		public AckRunnable(final IMessage origMessage) {
@@ -43,7 +49,7 @@ public class HS485Impl implements HS485 {
 
 	}
 
-	class DataHandler implements RawDataHandler {
+	private class DataHandler implements RawDataHandler {
 
 		public void handleClientList(final List<Integer> clients) {
 			synchronized (clientListMutex) {
@@ -94,21 +100,13 @@ public class HS485Impl implements HS485 {
 						}
 					}
 				} else if (packet.getTargetAddress() == ownAddress) {
-					synchronized (expectedReceiveQueue) {
-						LinkedList<IMessage> queue = expectedReceiveQueue.get(iMsg.getSourceAddress());
-						if (queue == null) {
-							queue = new LinkedList<IMessage>();
-							expectedReceiveQueue.put(iMsg.getSourceAddress(), queue);
-						}
-						queue.addLast(iMsg);
-					}
-					executorService.execute(new Runnable() {
-						public void run() {
-							synchronized (expectedReceiveQueue) {
-								expectedReceiveQueue.notifyAll();
-							}
-						}
-					});
+					final AckIndex key = new AckIndex(iMsg.getSourceAddress(), iMsg.getReceiveNumber());
+					final BlockingQueue<IMessage> newQueue = new LinkedBlockingQueue<IMessage>();
+					final BlockingQueue<IMessage> oldQueue = expectedReceiveQueue.putIfAbsent(key, newQueue);
+					if (oldQueue != null)
+						oldQueue.add(iMsg);
+					else
+						newQueue.add(iMsg);
 				} else if (packet.getTargetAddress() == BROADCAST_ADDRESS)
 					handleBroadcast(iMsg);
 
@@ -116,25 +114,10 @@ public class HS485Impl implements HS485 {
 				final ACKMessage ackPacket = (ACKMessage) packet; // handle
 				// ack-Message
 				final AckIndex index = new AckIndex(ackPacket.getSourceAddress(), ackPacket.getReceiveNumber());
-				synchronized (receivedAckCount) {
-					final Integer countBefore = receivedAckCount.get(index);
-					if (countBefore == null)
-						receivedAckCount.put(index, 1);
-					else
-						receivedAckCount.put(index, countBefore + 1);
-				}
-				executorService.execute(new Runnable() {
-					public void run() {
-						synchronized (receivedAckCount) {
-							receivedAckCount.notifyAll();
-						}
-					}
-				});
+				receivedAckMessages.putIfAbsent(index, new LinkedBlockingQueue<ACKMessage>());
+				final BlockingQueue<ACKMessage> queue = receivedAckMessages.get(index);
+				queue.add(ackPacket);
 			}
-			/*
-			 * } else if (packet.getTargetAddress() == 0xffffffff) { if (packet
-			 * instanceof IMessage) { IMessage iMsg = (IMessage) packet; }
-			 */
 		}
 
 		private void doAck(final IMessage iMsg) {
@@ -142,42 +125,42 @@ public class HS485Impl implements HS485 {
 		}
 	}
 
-	private static final int												BROADCAST_ADDRESS			= 0xffffffff;
+	private static final int																		BROADCAST_ADDRESS			= 0xffffffff;
 
-	private static final int												INC_REPEAT_COUNT			= 8;
+	private static final int																		INC_REPEAT_COUNT			= 8;
 
-	private static final Logger											log										= LoggerFactory.getLogger(HS485Impl.class);
+	private static final Logger																	log										= LoggerFactory.getLogger(HS485Impl.class);
 
-	private static final int												PACKET_REPEAT_COUNT		= 4;
+	private static final int																		PACKET_REPEAT_COUNT		= 4;
 
-	private static final int												PACKET_WAIT_TIME			= 200;
+	private static final int																		PACKET_WAIT_TIME			= 200;
 
-	private final List<BroadcastHandler>						broadcastHandlers			= new ArrayList<BroadcastHandler>();
+	private final Collection<BroadcastHandler>									broadcastHandlers			= new ConcurrentLinkedQueue<BroadcastHandler>();
 
-	private List<Integer>														clientList;
+	private List<Integer>																				clientList;
 
-	private final Object														clientListMutex				= new Object();
+	private final Object																				clientListMutex				= new Object();
 
-	private RXTXPort																commPort;
+	private RXTXPort																						commPort;
 
-	private int																			currentSenderNumber;
+	private int																									currentSenderNumber;
 
-	private PacketDecoder														decoder;
+	private PacketDecoder																				decoder;
 
-	private PacketEncoder														encoder;
+	private PacketEncoder																				encoder;
 
-	private ExecutorService													executorService				= Executors.newCachedThreadPool();
+	private ExecutorService																			executorService				= Executors.newCachedThreadPool();
 
-	private HashMap<Integer, LinkedList<IMessage>>	expectedReceiveQueue;
+	private ConcurrentMap<AckIndex, BlockingQueue<IMessage>>		expectedReceiveQueue;
 
-	private HashMap<EventIndex, EventHandler>				keyEventHandlers;
+	private Map<EventIndex, EventHandler>												keyEventHandlers;
 
-	private long																		lastClientListUpdate	= 0;
+	private long																								lastClientListUpdate	= 0;
 
-	private int																			ownAddress;
-	private final Object														readClientListMutex		= new Object();
+	private int																									ownAddress;
+	private final Object																				readClientListMutex		= new Object();
 
-	private HashMap<AckIndex, Integer>							receivedAckCount;
+	private ConcurrentMap<AckIndex, BlockingQueue<ACKMessage>>	receivedAckMessages;
 
 	public HS485Impl(final String port, final int myAddress) throws UnsupportedCommOperationException, IOException {
 		init(port, myAddress);
@@ -185,43 +168,24 @@ public class HS485Impl implements HS485 {
 	}
 
 	public void addBroadcastHandler(final BroadcastHandler handler) {
-		synchronized (broadcastHandlers) {
-			broadcastHandlers.add(handler);
-		}
+		broadcastHandlers.add(handler);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#addKeyHandler(int, byte,
-	 * ch.eleveneye.hs485.event.EventHandler)
-	 */
 	public void addKeyHandler(final int targetAddress, final byte actorNr, final EventHandler handler) throws IOException {
-		synchronized (keyEventHandlers) {
-			final EventIndex eventIndex = new EventIndex(targetAddress, actorNr);
-			keyEventHandlers.put(eventIndex, handler);
-		}
-		// registerEventAt(targetAddress, actorNr, (byte) 0);
+		final EventIndex eventIndex = new EventIndex(targetAddress, actorNr);
+		keyEventHandlers.put(eventIndex, handler);
 	}
 
 	public void handleBroadcast(final IMessage iMsg) {
 		executorService.execute(new Runnable() {
 
 			public void run() {
-				synchronized (broadcastHandlers) {
-					for (final BroadcastHandler handler : broadcastHandlers)
-						handler.handleBroadcastMessage(iMsg);
-
-				}
+				for (final BroadcastHandler handler : broadcastHandlers)
+					handler.handleBroadcastMessage(iMsg);
 			}
 		});
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#listClients()
-	 */
 	public List<Integer> listClients() throws IOException {
 		synchronized (readClientListMutex) {
 
@@ -248,67 +212,38 @@ public class HS485Impl implements HS485 {
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#listOwnAddresse()
-	 */
 	public int[] listOwnAddresse() {
 		return new int[] { ownAddress };
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#readActor(int, byte)
-	 */
 	public byte readActor(final int moduleAddress, final byte actor) throws IOException {
 		final IMessage msg = prepareIMessage(moduleAddress);
 		final byte[] data = new byte[] { 'S', actor };
 		msg.setData(data);
 		final IMessage answer = sendAndWaitForAnswer(msg);
 
-		sendAck(answer);
-
 		return answer.getData()[1];
 
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#readHwVer(int)
-	 */
 	public HwVer readHwVer(final int address) throws IOException {
 		final IMessage msg = prepareIMessage(address);
 		msg.setData(new byte[] { 'h' });
 		final IMessage answer = sendAndWaitForAnswer(msg);
-		sendAck(answer);
 
 		final byte[] answerData = answer.getData();
 		return new HwVer(answerData[0], answerData[1]);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#readLux(int)
-	 */
 	public int readLux(final int address) throws IOException {
 		final IMessage msg = prepareIMessage(address);
 		msg.setData(new byte[] { 'L' });
 		final IMessage answer = sendAndWaitForAnswer(msg);
-		sendAck(answer);
 
 		final byte[] answerData = answer.getData();
 		return (answerData[0] & 0xff) << 24 | (answerData[1] & 0xff) << 16 | (answerData[2] & 0xff) << 8 | answerData[3] & 0xff;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#readModuleEEPROM(int, int)
-	 */
 	public byte[] readModuleEEPROM(final int address, final int count) throws IOException {
 		final byte[] ret = new byte[count];
 		int partOffset = 0;
@@ -322,16 +257,10 @@ public class HS485Impl implements HS485 {
 		return ret;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#readSwVer(int)
-	 */
 	public SwVer readSwVer(final int address) throws IOException {
 		final IMessage msg = prepareIMessage(address);
 		msg.setData(new byte[] { 'v' });
 		final IMessage answer = sendAndWaitForAnswer(msg);
-		sendAck(answer);
 
 		final byte[] answerData = answer.getData();
 		if (answerData.length < 1)
@@ -342,16 +271,10 @@ public class HS485Impl implements HS485 {
 		return new SwVer(answerData[0], answerData[1]);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#readTemp(int)
-	 */
 	public TFSValue readTemp(final int address) throws IOException {
 		final IMessage msg = prepareIMessage(address);
 		msg.setData(new byte[] { 'F' });
 		final IMessage answer = sendAndWaitForAnswer(msg);
-		sendAck(answer);
 
 		final byte[] answerData = answer.getData();
 		final TFSValue value = new TFSValue(answerData);
@@ -360,12 +283,6 @@ public class HS485Impl implements HS485 {
 		return value;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#registerEventAt(int, byte,
-	 * byte)
-	 */
 	public void registerEventAt(final int moduleAddress, final byte sensor, final byte actor) throws IOException {
 		final IMessage msg = prepareIMessage(moduleAddress);
 		final byte[] data = new byte[] { 'q', sensor, actor };
@@ -373,22 +290,12 @@ public class HS485Impl implements HS485 {
 		sendAndWaitForAck(msg);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#reloadModule(int)
-	 */
 	public void reloadModule(final int address) throws IOException {
 		final IMessage msg = prepareIMessage(address);
 		msg.setData(new byte[] { 'C' });
 		sendAndWaitForAck(msg);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#resetModule(int)
-	 */
 	public void resetModule(final int address) throws IOException {
 		final IMessage msg = prepareIMessage(address);
 		msg.setData(new byte[] { '!', '!' });
@@ -399,12 +306,6 @@ public class HS485Impl implements HS485 {
 		this.executorService = executorService;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#unRegisterEventAt(int,
-	 * byte, byte)
-	 */
 	public void unRegisterEventAt(final int moduleAddress, final byte sensor, final byte actor) throws IOException {
 		final IMessage msg = prepareIMessage(moduleAddress);
 		final byte[] data = new byte[] { 'c', sensor, actor };
@@ -412,11 +313,6 @@ public class HS485Impl implements HS485 {
 		sendAndWaitForAck(msg);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#writeActor(int, byte, byte)
-	 */
 	public void writeActor(final int moduleAddress, final byte actor, final byte action) throws IOException {
 		final IMessage msg = prepareIMessage(moduleAddress);
 		final byte[] data = new byte[] { 's', 0, actor, action };
@@ -424,12 +320,6 @@ public class HS485Impl implements HS485 {
 		sendAndWaitForAck(msg);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see ch.eleveneye.hs485.protocol.HS485Interface#writeModuleEEPROM(int, int,
-	 * byte[], int, int)
-	 */
 	public void writeModuleEEPROM(final int deviceAddress, final int memOffset, final byte[] data, final int dataOffset, final int length)
 			throws IOException {
 		int partOffset = 0;
@@ -450,9 +340,9 @@ public class HS485Impl implements HS485 {
 
 	private void init(final String port, final int myAddress) throws UnsupportedCommOperationException, IOException {
 		ownAddress = myAddress;
-		receivedAckCount = new HashMap<AckIndex, Integer>();
-		expectedReceiveQueue = new HashMap<Integer, LinkedList<IMessage>>();
-		keyEventHandlers = new HashMap<EventIndex, EventHandler>();
+		receivedAckMessages = new ConcurrentHashMap<AckIndex, BlockingQueue<ACKMessage>>();
+		expectedReceiveQueue = new ConcurrentHashMap<AckIndex, BlockingQueue<IMessage>>();
+		keyEventHandlers = new ConcurrentHashMap<EventIndex, EventHandler>();
 
 		final RXTXCommDriver commDriver = new RXTXCommDriver();
 		commDriver.initialize();
@@ -487,7 +377,6 @@ public class HS485Impl implements HS485 {
 		final IMessage msg = prepareIMessage(address);
 		msg.setData(new byte[] { 'R', (byte) (offset >> 8 & 0xff), (byte) (offset & 0xff), (byte) (dataCount & 0xff) });
 		final IMessage answer = sendAndWaitForAnswer(msg);
-		sendAck(answer);
 		assert answer.getData().length == dataCount;
 		System.arraycopy(answer.getData(), 0, data, dataOffset, dataCount);
 	}
@@ -501,66 +390,62 @@ public class HS485Impl implements HS485 {
 		encoder.sendPacket(ack);
 	}
 
-	synchronized private void sendAndWaitForAck(final IMessage msg) throws IOException {
+	private ACKMessage sendAndWaitForAck(final IMessage msg) throws IOException {
 		final byte origSenderNumber = msg.getSenderNumber();
 		for (int k = 0; k < INC_REPEAT_COUNT; k++) {
 			final byte tempSenderNummer = (byte) (origSenderNumber + k & 0x3);
 			msg.setSenderNumber(tempSenderNummer);
 			final AckIndex ackIndex = new AckIndex(msg.getTargetAddress(), msg.getSenderNumber());
-			synchronized (receivedAckCount) {
-				receivedAckCount.put(ackIndex, 0);
-			}
-			for (int i = 0; i < PACKET_REPEAT_COUNT; i++) {
-				encoder.sendPacket(msg);
-				// for (int j = 0; j < PACKET_WAIT_TIME; j++) {
-				synchronized (receivedAckCount) {
+			receivedAckMessages.putIfAbsent(ackIndex, new LinkedBlockingQueue<ACKMessage>());
+			final BlockingQueue<ACKMessage> queue = receivedAckMessages.get(ackIndex);
+			synchronized (queue) {
+				// wait always once per target and ack-number
+				for (int i = 0; i < PACKET_REPEAT_COUNT; i++)
 					try {
-						receivedAckCount.wait(PACKET_WAIT_TIME);
-						// Thread.sleep(1);
+						encoder.sendPacket(msg);
+						ACKMessage ackMessage;
+						ackMessage = queue.poll(PACKET_WAIT_TIME, TimeUnit.MILLISECONDS);
+						if (ackMessage != null)
+							return ackMessage;
 					} catch (final InterruptedException e) {
 						log.warn("Exception beim Warten auf ACK", e);
 					}
-					if (receivedAckCount.get(ackIndex) > 0)
-						// System.out.println("time: " + j);
-						return;
-				}
-				// }
 			}
 		}
 		throw new TimeoutException("Expected Ack not requested");
 	}
 
-	synchronized private IMessage sendAndWaitForAnswer(final IMessage msg) throws IOException {
+	private IMessage sendAndWaitForAnswer(final IMessage msg) throws IOException {
 
 		final byte origSenderNumber = msg.getSenderNumber();
 		for (int k = 0; k < INC_REPEAT_COUNT; k++) {
 			final byte tempSenderNummer = (byte) (origSenderNumber + k & 0x3);
 			msg.setSenderNumber(tempSenderNummer);
 
-			synchronized (expectedReceiveQueue) {
-				expectedReceiveQueue.put(msg.getTargetAddress(), new LinkedList<IMessage>());
-			}
-			for (int i = 0; i < PACKET_REPEAT_COUNT; i++) {
-				encoder.sendPacket(msg);
-				// for (int j = 0; j < PACKET_WAIT_TIME; j++) {
-				synchronized (expectedReceiveQueue) {
+			final BlockingQueue<IMessage> newQueue = new LinkedBlockingQueue<IMessage>();
+			final BlockingQueue<IMessage> existingQueue = expectedReceiveQueue.putIfAbsent(new AckIndex(msg.getTargetAddress(), msg.getSenderNumber()),
+					newQueue);
+			final BlockingQueue<IMessage> receiveQueue;
+			if (existingQueue != null)
+				receiveQueue = existingQueue;
+			else
+				receiveQueue = newQueue;
+			synchronized (receiveQueue) {
+				// do not send more than one request concurrent to one target
+				for (int i = 0; i < PACKET_REPEAT_COUNT; i++) {
+					encoder.sendPacket(msg);
+
 					try {
-						expectedReceiveQueue.wait(PACKET_WAIT_TIME);
-						// Thread.sleep(1);
+						final IMessage answer = receiveQueue.poll(PACKET_WAIT_TIME, TimeUnit.MILLISECONDS);
+						if (answer != null) {
+							sendAck(answer);
+							return answer;
+						}
 					} catch (final InterruptedException e) {
 						log.warn("Exception beim Warten auf Antwort", e);
 					}
-					final LinkedList<IMessage> queue = expectedReceiveQueue.get(msg.getTargetAddress());
-					while (queue.size() > 0) {
-						final IMessage nextCandidate = queue.removeFirst();
-						if (nextCandidate.getReceiveNumber() == msg.getSenderNumber())
-							return nextCandidate;
-						else {
-							// log.warn("Wrong answer: " + nextCandidate);
-						}
-					}
+
 				}
-				// }
 			}
 		}
 		throw new TimeoutException("Expected Answer not requested");
